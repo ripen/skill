9f4d0bebc5a1061b5ca14b8abf47513e6410e29b
wangchegnzhen <wangchengzhen@sungmedical.com>
2011-09-01 10:05:47 +0800

    add function packege

---
+++ 常用函数、方法-可扩展，尽量采用此包！！/MY_global.func.php
@@ -0,0 +1,11 @@
+<?php
+/**
+* 添加描述
+*
+* @author RipenWang WangYangyang LiuXiaoBo LiuTao
+* @copyright ripen_wang@yahoo.com.cn wavabobo1@126.com wangyang8839@163.com station863@gmail.com
+* @version 1.0
+*/
+
+
+?>
